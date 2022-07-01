aedd520ade9feb33ed15249fd9b43ec8b32e0191
WIN-M1PEGDK0MK5\Anton <Anton.arlou@1pt.com>
2022-07-01 16:31:41 +0300

    add func

--- first_programm/nums.py
+++ first_programm/nums.py
@@ -1,7 +1,8 @@
 def summ(a,b):
     return a+b
 
-text = 'hello, world'
+def mult(a,b):
+    return a*b
 
 def minus(a,b):
     return a-b
@@ -9,3 +10,4 @@ def minus(a,b):
 if __name__ == '__main__':
     print(summ(4,5))
     print(minus(8,9))
+    print(mult(5,8))
