02d68fd69ef9f23bdaa0650d6f3593c1932fc754
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-07-18 14:37:04 +0300

    add function in the class

---
+++ first_programm/class.py
@@ -0,0 +1,9 @@
+class  Animal:
+    def __init__(self, head, legs, ears):
+        self.head = head
+        self.legs = legs
+        self.ears = ears
+
+    def voice(self, roar:str):
+        print(roar.upper)
+        
