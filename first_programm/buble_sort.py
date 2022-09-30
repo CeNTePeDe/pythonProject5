6f73a889cc825a18f6e314c5f1dd27ea616cb1f3
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-09-30 12:14:13 +0300

    add buble_sort to function via statment while

--- first_programm/buble_sort.py
+++ first_programm/buble_sort.py
@@ -11,3 +11,20 @@ for i in range(N-1):
    a[j], a[j+1] = a[j+1], a[j]
 
 print(a)
+
+N = 10
+a = []
+for i in range(N):
+ a.append(randint(1,99))
+print(a)
+
+i = 0
+while i < N-1:
+ j = 0
+ while j < N - 1-i:
+  if a[j] > a[j+1]:
+   a[j], a[j+1] = a[j+1], a[j]
+  j += 1
+ i += 1
+print(a)
+
