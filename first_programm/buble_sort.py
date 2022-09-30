2b72c7c9acf58798ad7f4454a23052431e7501e9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-09-30 12:21:00 +0300

    add buble_sort to function via array

--- first_programm/buble_sort.py
+++ first_programm/buble_sort.py
@@ -28,3 +28,20 @@ while i < N-1:
  i += 1
 print(a)
 
+def bobble(array):
+ for i in range(N-1):
+  for j in range(N-i-1):
+   if array[j] > array[j+1]:
+    buff = array[j]
+    array[j] = array[j+1]
+    array[j+1] = buff
+ return array
+
+
+N = 10
+
+print(a)
+for i in range(N):
+ a.append(randint(1,99))
+print(a)
+print(bobble(a))
