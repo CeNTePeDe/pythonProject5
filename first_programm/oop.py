aaf14649b0c97fdb2338161bd54c39724f3e3893
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-10-06 15:33:21 +0300

    add comments

--- first_programm/oop.py
+++ first_programm/oop.py
@@ -1,12 +1,13 @@
 class ContactList(list):
-# наследуется от кдасса list
-    def seach(self,name):
+    # наследуется от кдасса list
+    def seach(self, name):
         """Return all contacts that contain the search values in their name."""
         matching_contacts = []
         for contact in self:
             if name in contact.name:
                 matching_contacts.append(contact)
         return matching_contacts
+
 
 class Contact:
     all_contacts = ContactList()
@@ -16,12 +17,14 @@ class Contact:
         self.email = email
         Contact.all_contacts.append(self)
 
+
 class Suppliers(Contact):
     def order(self, order):
-         print(
+        print(
             "if this were a real system we would send "
             f"'{order}' order to '{self.name}'"
         )
+
 
 c1 = Contact('Misha 1', 'misha@mail.ru')
 c2 = Contact('Misha 2', 'misha2@mail.ru')
@@ -31,7 +34,7 @@ s2 = Suppliers(c2.name, c2.email)
 s3 = Suppliers(c3.name, c3.email)
 
 if __name__ == '__main__':
-    #print(Contact.all_contacts)
-    #print([c.name for c in Contact.all_contacts])
-    #print([c.email for c in Contact.all_contacts])
+    # print(Contact.all_contacts)
+    # print([c.name for c in Contact.all_contacts])
+    # print([c.email for c in Contact.all_contacts])
     print(s1.order('orders'))
