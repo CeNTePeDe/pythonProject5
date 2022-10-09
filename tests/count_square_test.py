dd9aeebba4aef1f2b79c2ae5a8d20ef38f2b7658
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-10-09 12:45:14 +0300

    add test exeption

--- tests/count_square_test.py
+++ tests/count_square_test.py
@@ -17,6 +17,18 @@ class SquareEqSolverTestCase(unittest.TestCase):
         self.assertEqual(len(res), 2)
         self.assertEqual(res, [0.5, -3])
 
+    def test_error(self):
+        with self.assertRaises(AttributeError) as e:
+            res = square_eq_solver(0.1, 0.5, -3)
+            self.assertEqual("вводимые данные должны быть целочисленные", e.exception.args[0])
+
+
+
+    #def no_sight(self):
+    #    with self.assertRaises(ValueError) as e:
+#
+
+
 
 if __name__ == '__main__':
     unittest.main()
