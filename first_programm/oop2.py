cce994da944f55b0a3e6fdcb8658ffeb7d3b6de7
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-10-21 10:04:04 +0300

    upgrate code with inheritance

--- first_programm/oop2.py
+++ first_programm/oop2.py
@@ -1,40 +1,30 @@
-class Cleaner:
-    def __init__(self, name):
+class Employee:
+    def __init__(self, name, salary, bonus):
         self.name = name
-        self.salary = 15000
-        self.bonus = 1
+        self.salary = salary
+        self.bonus = bonus
 
     def calculate_total_bonus(self):
         return self.salary // 100 * self.bonus
 
     def __str__(self):
-        return f'Cleaner {self.name}, salary = {self.salary}, bonus={self.bonus}%, total bonus = {self.calculate_total_bonus()} rub'
+        return f'{self.__class__.__name__} {self.name}, salary = {self.salary}, bonus={self.bonus}%, ' \
+               f'total bonus = {self.calculate_total_bonus()} rub'
 
 
-class Manager:
+class Cleaner(Employee):
     def __init__(self, name):
-        self.name = name
-        self.salary = 45000
-        self.bonus = 15
-
-    def calculate_total_bonus(self):
-        return self.salary // 100 * self.bonus
-
-    def __str__(self):
-        return f'Manager {self.name}, salary = {self.salary}, bonus={self.bonus}%, total bonus = {self.calculate_total_bonus()} rub'
+        super(Cleaner, self).__init__(name, salary=15000, bonus=1)
 
 
-class CEO:
+class Manager(Employee):
     def __init__(self, name):
-        self.name = name
-        self.salary = 105000
-        self.bonus = 100
+        super(Manager, self).__init__(name, salary=45000, bonus=15)
 
-    def calculate_total_bonus(self):
-        return self.salary // 100 * self.bonus
 
-    def __str__(self):
-        return f'CEO {self.name}, salary = {self.salary}, bonus={self.bonus}%, total bonus = {self.calculate_total_bonus()} rub'
+class CEO(Employee):
+    def __init__(self, name):
+        super(CEO, self).__init__(name, salary=105000, bonus=100)
 
 if __name__ == '__main__':
     masha = Cleaner('Maria Ivanovna')
