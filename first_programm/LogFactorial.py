698ccfbe315276b8745df69acb9aeebb1d85f8e9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-09 16:12:51 +0300

    func factorial with log

---
+++ first_programm/LogFactorial.py
@@ -0,0 +1,24 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Начало программы')
+
+
+def factorial(n: int):
+    """
+    функция высчитывает факториалы от числа n
+    :param n: int
+    :return: total: int
+    """
+    logging.debug('Начало factorial(%s%%)' % (n))
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug(f'i = {i}, total = {total}')
+    logging.debug('Конец factorial(%s%%)' % (n))
+    return total
+
+
+if __name__ == '__main__':
+    print(factorial(5))
+    logging.debug('end')
